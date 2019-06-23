6cdcb72853f7ccc9801670c467972100aadbbb87
takayama naoki <ubu.linux.beginner@gmail.com>
2019-06-23 22:53:17 +0900

    jQuery有無を判定し、無ければCDNから読み込む

--- bookmarklet/load_jquery.js
+++ bookmarklet/load_jquery.js
@@ -2,6 +2,7 @@ javascript:(function() {
   console.log("LOG-START----------");
   jquery_url = "https://code.jquery.com/jquery-3.1.0.slim.min.js";
 
+  // jQuery有無を判定
   if (typeof jQuery == "undefined") {
       console.log("jquery not loaded.");
   } else {
@@ -10,12 +11,14 @@ javascript:(function() {
       return true;
   }
 
+  // jQueryをCDNから読み込む
   var s = document.createElement("script"); 
   s.type = "text/javascript"; 
   s.src = jquery_url; 
   console.log(s);
   document.body.appendChild(s);
 
+  // 読み込み結果を判定
   s.onload = function() { 
       if (typeof jQuery == "undefined") { 
           console.log("jquery load failed!");
